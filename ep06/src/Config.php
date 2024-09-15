810371a681ed031524729b0eb6c499ed15276b3a
breenoox <106892141+breenoox@users.noreply.github.com>
2024-09-15 00:08:52 -0300

    inicio ep06

---
+++ ep06/src/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+define("FACEBOOK", [
+    "app_id" => "",
+    "app_secret" => "",
+    "app_redirect" => "http://localhost/phptips/ep06",
+    "app_version" => "v4.0"
+]);
+
+/*
+
+'clientId'     => '{google-client-id}',
+'clientSecret' => '{google-client-secret}',
+'redirectUri'  => 'https://example.com/callback-url',
+'hostedDomain' => 'example.com',
+
+*/
